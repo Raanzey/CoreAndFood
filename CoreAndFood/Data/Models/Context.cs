9c2b6c8f9074379f56eab4c8ab050effce89b35f
yusuf taha özlü <yutahaozlu@gmail.com>
2024-06-19 17:06:50 +0300

    account

--- CoreAndFood/Data/Models/Context.cs
+++ CoreAndFood/Data/Models/Context.cs
@@ -11,5 +11,20 @@ namespace CoreAndFood.Data.Models
         public DbSet<Food> Foods { get; set; }
         public DbSet<Category> Categories { get; set; } 
         public DbSet<Account> Accounts { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // Başlangıç verisi ekleme
+            modelBuilder.Entity<Account>().HasData(new Account
+            {
+                AccountID = 1,
+                UserName = "test",
+                Email = "test@gmail.com",
+                Password = "test123",
+                Role = "Admin"
+            });
+        }
     }
 }
---
+++ CoreAndFood/Migrations/20240619140206_Account.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CoreAndFood.Migrations
+{
+    public partial class Account : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Accounts",
+                columns: new[] { "AccountID", "Email", "Password", "Role", "UserName" },
+                values: new object[] { 1, "test@gmail.com", "test123", "Admin", "test" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Accounts",
+                keyColumn: "AccountID",
+                keyValue: 1);
+        }
+    }
+}
